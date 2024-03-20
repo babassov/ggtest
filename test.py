7827a1bdf325991460398cb97a7b0c2d1dae7d3d
Vincent Boyer <boyer.vincent@gmail.com>
2024-03-20 10:27:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
  database = aws_lib.connect("AKIAF6BASJRR45SBWZZ0", "hjshnk5ex5u34565DWS654/SKGjhz595d89sjkje")
+xcvxcvxcv
+cv
+xcv
+xcvxcvxcvxcv
+xcv
+xxcvxcvxcv
+xcvxcv
+xcvxcv
+cxvxcvxc
+xcvxcvxcvxcvds
+fsdf
+
+ database = aws_lib.connect("AKIAF6BASJRR45SBWZZ0", "hjshnk5ex5u34565DWS654/SKGjhz595d89sjkje")
